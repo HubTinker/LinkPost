a6ea31e61302c887ec989aea337b77a12d9a3aee
HubTinker <dmaxart@gmail.com>
2026-06-25 11:48:11 +0300

    fix: switch from Edge to Node.js runtime for platform-api2 TLS compatibility

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ import {
   saveUser, getUserCount, reactivateUser, checkRateLimit
 } from '../lib/storage.js'
 
-export const config = { runtime: 'edge' }
+export const config = { runtime: 'nodejs20.x' }
 
 const app = new Hono()
 
